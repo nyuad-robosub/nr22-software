3bd0267415a868088d77f3bc0039cb034edd86e3
nhamngocminhquan <nhamngocminhquan@gmail.com>
2022-04-30 02:58:50 -0400

    Add filters to point cloud node

---
+++ src/avoid_obstacles/scripts/tf2_pointcloud.cpp
@@ -0,0 +1,74 @@
+// Point cloud transform with tf2 courtesy of:
+// https://github.com/stereolabs/zed-ros-wrapper/issues/393#issuecomment-478958782
+// https://answers.ros.org/question/262179/transform-pointcloud-with-tf2/
+
+#include <ros/ros.h>
+
+// PCL specific includes
+#include <sensor_msgs/PointCloud2.h>
+
+// TF2 includes
+#include <tf2/transform_datatypes.h>
+#include <tf2/convert.h>
+#include <tf2_ros/buffer.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_sensor_msgs/tf2_sensor_msgs.h>
+#include <geometry_msgs/TransformStamped.h>
+
+// Global transformStamped
+geometry_msgs::TransformStamped transformStamped;
+
+// Global publisher
+ros::Publisher pub;
+
+// Callback for cloud listener
+void cloud_callback (const sensor_msgs::PointCloud2& cloud_in) {
+    std::cout << transformStamped << std::endl;
+
+    // Do transform
+    sensor_msgs::PointCloud2 cloud_out;
+    tf2::doTransform(cloud_in, cloud_out, transformStamped);
+
+    // Publish cloud
+    cloud_out.header = cloud_in.header;
+    pub.publish(cloud_out);
+}
+
+
+int main(int argc, char** argv) {
+    // Initialize ROS
+    ROS_INFO("Node started");
+
+    ros::init (argc, argv, "point_cloud_transform");
+    ros::NodeHandle nh;
+
+    // Create a ROS subscriber for the input point cloud
+    ros::Subscriber sub = nh.subscribe ("/camera/depth/color/points", 1, cloud_callback);
+
+    // Create a ROS publisher for the output point cloud
+    pub = nh.advertise<sensor_msgs::PointCloud2> ("/point_cloud/cloud_transformed", 1);
+
+    tf2_ros::Buffer tfBuffer;
+    tf2_ros::TransformListener tfListener(tfBuffer);
+    // sensor_msgs::PointCloud2 cloud_in, cloud_out;
+
+    ros::Rate rate(30.0);
+    while (nh.ok()){
+        try{
+            transformStamped = tfBuffer.lookupTransform("world", "camera_depth_frame",
+                ros::Time(0));
+        }
+        catch (tf2::TransformException ex){
+            ROS_ERROR("%s",ex.what());
+            // ros::Duration(1.0).sleep();
+            continue;
+        }
+
+        ros::spinOnce();
+        rate.sleep();
+    }
+
+    ros::spin();
+    return 0;
+}
--- src/avoid_obstacles/scripts/tf_pointcloud.cpp
+++ src/avoid_obstacles/scripts/tf_pointcloud.cpp
@@ -3,38 +3,59 @@
 
 #include <ros/ros.h>
 #include <ros/console.h>
+
 // PCL specific includes
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
-#include <pcl/ModelCoefficients.h>
-#include <pcl/io/pcd_io.h>
-#include <pcl/point_types.h>
-#include <pcl/filters/extract_indices.h>
-#include <pcl/sample_consensus/method_types.h>
-#include <pcl/sample_consensus/model_types.h>
-#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/filters/statistical_outlier_removal.h>
+#include <pcl/filters/voxel_grid.h>
 #include <pcl_ros/transforms.h>
 
-
+// Global publisher
 ros::Publisher pub;
 tf::StampedTransform transform;
 
+// Callback for cloud listener
 void cloud_callback (const sensor_msgs::PointCloud2ConstPtr& input)
 {
     // Create a container for the data.
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_transformed(new pcl::PointCloud<pcl::PointXYZ>);
-    pcl::PointCloud<pcl::PointXYZ>::Ptr temp_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_transformed(new pcl::PointCloud<pcl::PointXYZ>),
+                                        temp1_cloud(new pcl::PointCloud<pcl::PointXYZ>),
+                                        temp2_cloud(new pcl::PointCloud<pcl::PointXYZ>),
+                                        temp3_cloud(new pcl::PointCloud<pcl::PointXYZ>),
+                                        cloud_filtered(new pcl::PointCloud<pcl::PointXYZ>);
 
-    pcl::PCLPointCloud2 pcl_pc2;
-    pcl_conversions::toPCL(*input, pcl_pc2);
-    pcl::fromPCLPointCloud2(pcl_pc2,*temp_cloud);
+    // pcl::PCLPointCloud2 pcl_pc2;
+    // pcl_conversions::toPCL(*input, pcl_pc2);
+    // pcl::fromPCLPointCloud2(pcl_pc2,*temp_cloud);
+    pcl::fromROSMsg (*input, *temp1_cloud);
 
-    pcl_ros::transformPointCloud(*temp_cloud, *cloud_transformed, transform);
+    // Filter for stray points
+    pcl::StatisticalOutlierRemoval<pcl::PointXYZ> sor;
+    sor.setInputCloud (temp1_cloud);
+    sor.setMeanK (25);
+    sor.setStddevMulThresh (1.0);
+    sor.filter (*temp2_cloud);
+
+    // Downsample filter for voxels: 5cm voxels
+    pcl::VoxelGrid<pcl::PointXYZ> vg1;
+    vg1.setInputCloud (temp2_cloud);
+    vg1.setLeafSize (0.02f, 0.02f, 0.02f);
+    vg1.filter (*temp3_cloud);
+
+    // Transform the pointcloud
+    pcl_ros::transformPointCloud(*temp3_cloud, *cloud_transformed, transform);
+
+    // Downsample filter for voxels: 10cm voxels
+    pcl::VoxelGrid<pcl::PointXYZ> vg2;
+    vg2.setInputCloud (cloud_transformed);
+    vg2.setLeafSize (0.1f, 0.1f, 0.1f);
+    vg2.filter (*cloud_filtered);
 
     sensor_msgs::PointCloud2 cloud_publish;
-    pcl::toROSMsg(*cloud_transformed,cloud_publish);
+    pcl::toROSMsg (*cloud_filtered, cloud_publish);
     cloud_publish.header = input->header;
 
     pub.publish(cloud_publish);

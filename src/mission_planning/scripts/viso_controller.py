1fee3f944114ce8f7da57c0f1de1cb9eb2c7d26c
Nasheed Ur Rehman <96262883+nasheed-x@users.noreply.github.com>
2022-07-16 13:07:05 +0400

    Combined marker orientation function

--- src/mission_planning/scripts/viso_controller.py
+++ src/mission_planning/scripts/viso_controller.py
@@ -460,76 +460,9 @@ class viso_controller():
         # Check if there were any issues
         return (counter < timeout / delay)
     
-<<<<<<< HEAD
     def getOrientation(img_source):
     
         cnt=[]
-=======
-    # drawAxis function definition
-    def drawAxis(img, p_, q_, color, scale):
-        p = list(p_)
-        q = list(q_)
-
-        ## [visualization1]
-        angle = atan2(p[1] - q[1], p[0] - q[0]) # angle in radians
-        hypotenuse = sqrt((p[1] - q[1]) * (p[1] - q[1]) + (p[0] - q[0]) * (p[0] - q[0]))
-
-        # Here we lengthen the arrow by a factor of scale
-        q[0] = p[0] - scale * hypotenuse * cos(angle)
-        q[1] = p[1] - scale * hypotenuse * sin(angle)
-        cv2.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), color, 3, cv2.LINE_AA)
-
-        # create the arrow hooks
-        p[0] = q[0] + 9 * cos(angle + pi / 4)
-        p[1] = q[1] + 9 * sin(angle + pi / 4)
-        cv2.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), color, 3, cv2.LINE_AA)
-
-        p[0] = q[0] + 9 * cos(angle - pi / 4)
-        p[1] = q[1] + 9 * sin(angle - pi / 4)
-        cv2.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), color, 3, cv2.LINE_AA)
-        ## [visualization1]
-
-    # getOrientation function definition
-    def getOrientation(self,pts, img):
-        ## [pca]
-        # Construct a buffer used by the pca analysis
-        sz = len(pts)
-        data_pts = np.empty((sz, 2), dtype=np.float64)
-        for i in range(data_pts.shape[0]):
-            data_pts[i,0] = pts[i,0,0]
-            data_pts[i,1] = pts[i,0,1]
-
-        # perform PCA analysis
-        mean = np.empty((0))
-        mean, eigenvectors, eigenvalues = cv2.PCACompute2(data_pts, mean)
-
-        # store the center of the object
-        cntr = (int(mean[0,0]), int(mean[0,1]))
-        ## [pca]
-
-        ## [visualization]
-        # draw the principal components
-        cv2.circle(img, cntr, 3, (255, 0, 255), 2)
-        p1 = (cntr[0] + 0.02 * eigenvectors[0,0] * eigenvalues[0,0], cntr[1] + 0.02 * eigenvectors[0,1] * eigenvalues[0,0])
-        p2 = (cntr[0] - 0.02 * eigenvectors[1,0] * eigenvalues[1,0], cntr[1] - 0.02 * eigenvectors[1,1] * eigenvalues[1,0])
-        drawAxis(img, cntr, p1, (255, 255, 0), -1)
-        drawAxis(img, cntr, p2, (0, 0, 255), -5)
-
-        angle = atan2(eigenvectors[0,1], eigenvectors[0,0]) # orientation in radians
-        ## [visualization]
-
-        # label with the rotation angle
-        label = "  Rotation Angle: " + str(int(np.rad2deg(angle)) + 90) + " degrees"
-        textbox = cv2.rectangle(img, (cntr[0], cntr[1]-25), (cntr[0] + 250, cntr[1] + 10), (255,255,255), -1)
-        cv2.putText(img, label, (cntr[0], cntr[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
-
-        return angle
-
-
-    def process(self,img_source):
-
-        cnt = []
->>>>>>> af296e4f0038d11b31a538458aadc80c2473d37f
 
         path = img_source
         # Captures the live stream frame-by-frame
@@ -630,12 +563,7 @@ class viso_controller():
 
 
         # get angle
-<<<<<<< HEAD
         rotation = int(np.rad2deg(angle)) + 90
-=======
-        a = self.getOrientation(cnt, canvas)
-        rotation = int(np.rad2deg(a)) + 90
->>>>>>> af296e4f0038d11b31a538458aadc80c2473d37f
 
         cv2.imwrite('/Users/nasheed-x/Desktop/Orientation/test.jpeg', canvas)
 

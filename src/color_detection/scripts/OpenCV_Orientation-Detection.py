a11bdc9024afdb15c36128dbb9c6029aa5d34311
Nasheed Ur Rehman <96262883+nasheed-x@users.noreply.github.com>
2022-07-18 22:45:35 +0400

    Implemented contour clustering.

--- src/color_detection/scripts/OpenCV_Orientation-Detection.py
+++ src/color_detection/scripts/OpenCV_Orientation-Detection.py
@@ -10,71 +10,49 @@ import matplotlib.pyplot as plt
 import numpy as np
 from math import atan2, cos, sin, sqrt, pi
 
-# drawAxis function definition
-def drawAxis(img, p_, q_, color, scale):
-  p = list(p_)
-  q = list(q_)
- 
-  ## [visualization1]
-  angle = atan2(p[1] - q[1], p[0] - q[0]) # angle in radians
-  hypotenuse = sqrt((p[1] - q[1]) * (p[1] - q[1]) + (p[0] - q[0]) * (p[0] - q[0]))
- 
-  # Here we lengthen the arrow by a factor of scale
-  q[0] = p[0] - scale * hypotenuse * cos(angle)
-  q[1] = p[1] - scale * hypotenuse * sin(angle)
-  cv2.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), color, 3, cv2.LINE_AA)
- 
-  # create the arrow hooks
-  p[0] = q[0] + 9 * cos(angle + pi / 4)
-  p[1] = q[1] + 9 * sin(angle + pi / 4)
-  cv2.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), color, 3, cv2.LINE_AA)
- 
-  p[0] = q[0] + 9 * cos(angle - pi / 4)
-  p[1] = q[1] + 9 * sin(angle - pi / 4)
-  cv2.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), color, 3, cv2.LINE_AA)
-  ## [visualization1]
+def calculate_contour_distance(contour1, contour2): 
+    x1, y1, w1, h1 = cv2.boundingRect(contour1)
+    c_x1 = x1 + w1/2
+    c_y1 = y1 + h1/2
 
-# getOrientation function definition
-def getOrientation(pts, img):
-  ## [pca]
-  # Construct a buffer used by the pca analysis
-  sz = len(pts)
-  data_pts = np.empty((sz, 2), dtype=np.float64)
-  for i in range(data_pts.shape[0]):
-    data_pts[i,0] = pts[i,0,0]
-    data_pts[i,1] = pts[i,0,1]
- 
-  # perform PCA analysis
-  mean = np.empty((0))
-  mean, eigenvectors, eigenvalues = cv2.PCACompute2(data_pts, mean)
- 
-  # store the center of the object
-  cntr = (int(mean[0,0]), int(mean[0,1]))
-  ## [pca]
- 
-  ## [visualization]
-  # draw the principal components
-  cv2.circle(img, cntr, 3, (255, 0, 255), 2)
-  p1 = (cntr[0] + 0.02 * eigenvectors[0,0] * eigenvalues[0,0], cntr[1] + 0.02 * eigenvectors[0,1] * eigenvalues[0,0])
-  p2 = (cntr[0] - 0.02 * eigenvectors[1,0] * eigenvalues[1,0], cntr[1] - 0.02 * eigenvectors[1,1] * eigenvalues[1,0])
-  drawAxis(img, cntr, p1, (255, 255, 0), -1)
-  drawAxis(img, cntr, p2, (0, 0, 255), -5)
- 
-  angle = atan2(eigenvectors[0,1], eigenvectors[0,0]) # orientation in radians
-  ## [visualization]
- 
-  # label with the rotation angle
-  label = "  Rotation Angle: " + str(int(np.rad2deg(angle)) + 90) + " degrees"
-  textbox = cv2.rectangle(img, (cntr[0], cntr[1]-25), (cntr[0] + 250, cntr[1] + 10), (255,255,255), -1)
-  cv2.putText(img, label, (cntr[0], cntr[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
- 
-  return angle
+    x2, y2, w2, h2 = cv2.boundingRect(contour2)
+    c_x2 = x2 + w2/2
+    c_y2 = y2 + h2/2
 
+    return max(abs(c_x1 - c_x2) - (w1 + w2)/2, abs(c_y1 - c_y2) - (h1 + h2)/2)
 
-def process(img_source):
+def merge_contours(contour1, contour2):
+    return np.concatenate((contour1, contour2), axis=0)
+
+def agglomerative_cluster(contours, threshold_distance):
+    current_contours = contours
+    while len(current_contours) > 1:
+        min_distance = None
+        min_coordinate = None
+
+        for x in range(len(current_contours)-1):
+            for y in range(x+1, len(current_contours)):
+                distance = calculate_contour_distance(current_contours[x], current_contours[y])
+                if min_distance is None:
+                    min_distance = distance
+                    min_coordinate = (x, y)
+                elif distance < min_distance:
+                    min_distance = distance
+                    min_coordinate = (x, y)
+
+        if min_distance < threshold_distance:
+            index1, index2 = min_coordinate
+            current_contours[index1] = merge_contours(current_contours[index1], current_contours[index2])
+            del current_contours[index2]
+        else: 
+            break
+
+    return current_contours
+
+def getOrientation(img_source):
   
-  cnt = []
-  
+  cnt=[]
+
   path = img_source
   # Captures the live stream frame-by-frame
   frame = cv2.imread(path)
@@ -83,7 +61,7 @@ def process(img_source):
   hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
       
   # lower bound and upper bound for orange color
-  lower_bound = np.array([0,120,135])
+  lower_bound = np.array([0,120,60])
   upper_bound = np.array([65,255,255])
 
   #find color within the boundaries
@@ -100,12 +78,10 @@ def process(img_source):
   canvas = np.zeros(frame.shape)
   canvas.fill(0)
 
-  #create segmented image
-  segmented_img = cv2.bitwise_and(canvas, canvas, mask=mask)
-
   # find contours from the mask
-  contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-  cont_output = cv2.drawContours(segmented_img, contours, -1, (0, 255, 0), 3)
+  contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+  clust_cnt = agglomerative_cluster(list(contours), 40)
+  cnt = tuple(clust_cnt)
 
   # get minAreaRect of contour with max area
   max_area = -1
@@ -116,6 +92,10 @@ def process(img_source):
     if area>max_area:
       cnt = contours[i]
       max_area = area
+
+  if len(cnt) == 0:
+    print("No Marker Detected")
+    exit()
 
   areas = [cv2.contourArea(c) for c in contours]
   if (len(areas) !=0):
@@ -137,13 +117,47 @@ def process(img_source):
   #draw at center point
   canvas = cv2.circle(canvas, (x,y), 10, (0,255,0), -5)
 
+  ## [pca]
+  # Construct a buffer used by the pca analysis
+  sz = len(cnt)
+  data_pts = np.empty((sz, 2), dtype=np.float64)
+  for i in range(data_pts.shape[0]):
+    data_pts[i,0] = cnt[i,0,0]
+    data_pts[i,1] = cnt[i,0,1]
+ 
+  # perform PCA analysis
+  mean = np.empty((0))
+  mean, eigenvectors, eigenvalues = cv2.PCACompute2(data_pts, mean)
+ 
+  # store the center of the object
+  cntr = (int(mean[0,0]), int(mean[0,1]))
+  ## [pca]
+ 
+  ## [visualization]
+  # draw the principal components
+  cv2.circle(canvas, cntr, 3, (255, 0, 255), 2)
+  p1 = (cntr[0] + 0.02 * eigenvectors[0,0] * eigenvalues[0,0], cntr[1] + 0.02 * eigenvectors[0,1] * eigenvalues[0,0])
+  p2 = (cntr[0] - 0.02 * eigenvectors[1,0] * eigenvalues[1,0], cntr[1] - 0.02 * eigenvectors[1,1] * eigenvalues[1,0])
+
+ 
+  angle = atan2(eigenvectors[0,1], eigenvectors[0,0]) # orientation in radians
+  ## [visualization]
+ 
+  # label with the rotation angle
+  label = "  Rotation Angle: " + str(int(np.rad2deg(angle)) + 90) + " degrees"
+  textbox = cv2.rectangle(canvas, (cntr[0], cntr[1]-25), (cntr[0] + 250, cntr[1] + 10), (255,255,255), -1)
+  cv2.putText(canvas, label, (cntr[0], cntr[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+
+
   # get angle
-  a = getOrientation(cnt, canvas)
-  rotation = int(np.rad2deg(a)) + 90
+  rotation = int(np.rad2deg(angle)) + 90
+  if rotation >= 180:
+    rotation=rotation-180
 
   cv2.imwrite('/Users/nasheed-x/Desktop/Orientation/test.jpeg', canvas)
 
   return rotation
 
-a = process('/Users/nasheed-x/Desktop/Orientation/orange.jpeg')
+a = getOrientation('/Users/nasheed-x/Desktop/Orientation/orange2.jpeg')
 print(a) 
+

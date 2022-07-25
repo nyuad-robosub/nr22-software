ccb7ccdbf4eab72502dce1434eb828f704d8cb91
Nasheed Ur Rehman <96262883+nasheed-x@users.noreply.github.com>
2022-07-25 13:53:41 +0400

    Update marker.py

--- src/mission_planning/scripts/states/marker.py
+++ src/mission_planning/scripts/states/marker.py
@@ -79,19 +79,22 @@ class marker(smach.State):
 
         return "outcome1"
            #else get bbox angle and use trigonometry to determine translation amount return adjustment(self,detection)
+           
     def getMarkerOrientation(self):
-        
-        cnt=[]
 
-        # Captures the live stream frame-by-frame
+        cnt = []
+
         frame = vs.bottom_camera.get_cv_img()
                 
-        # Converts images from BGR to HSV 
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
             
         # lower bound and upper bound for orange color
         lower_bound = np.array([0,120,60])
         upper_bound = np.array([65,255,255])
+
+        #create canvas
+        canvas = np.zeros(frame.shape)
+        canvas.fill(0)
 
         #find color within the boundaries
         mask = cv2.inRange(hsv, lower_bound, upper_bound)
@@ -103,87 +106,74 @@ class marker(smach.State):
         mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
         mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
 
-        #create canvas
-        canvas = np.zeros(frame.shape)
-        canvas.fill(0)
-
         # find contours from the mask
         contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        clust_cnt = agglomerative_cluster(list(contours), 40)
-        cnt = tuple(clust_cnt)
+        cluster = agglomerative_cluster(list(contours), 40)
 
         # get minAreaRect of contour with max area
         max_area = -1
 
         #loop through contours
-        for i in range(len(contours)):
-            area = cv2.contourArea(contours[i])
+        for i in range(len(cluster)):
+            area = cv2.contourArea(cluster[i])
             if area>max_area:
-                cnt = contours[i]
+                cnt = cluster[i]
                 max_area = area
 
         if len(cnt) == 0:
             print("No Marker Detected")
             exit()
 
-        areas = [cv2.contourArea(c) for c in contours]
+        areas = [cv2.contourArea(c) for c in cluster]
+
         if (len(areas) !=0):
             max_index = np.argmax(areas)
-            cnt=contours[max_index]
+            cnt=cluster[max_index]
 
         #assign minAreaRect to contour with max area
         rect = cv2.minAreaRect(cnt)
         box = cv2.boxPoints(rect)
         box = np.int0(box)
 
-        #draw minAreaRect
-        cv2.drawContours(canvas,[box],0,(255, 0,0),2)
-        
         #center coordinates of min_rectangle also p1
         x = int(rect[0][0])
         y = int(rect[0][1])
 
-        #draw at center point
-        canvas = cv2.circle(canvas, (x,y), 10, (0,255,0), -5)
-
-        ## [pca]
-        # Construct a buffer used by the pca analysis
-        sz = len(cnt)
+        pts = cnt
+        sz = len(pts)
         data_pts = np.empty((sz, 2), dtype=np.float64)
         for i in range(data_pts.shape[0]):
-            data_pts[i,0] = cnt[i,0,0]
-            data_pts[i,1] = cnt[i,0,1]
-
-        # perform PCA analysis
+            data_pts[i,0] = pts[i,0,0]
+            data_pts[i,1] = pts[i,0,1]
+        
+        # Perform PCA analysis
         mean = np.empty((0))
         mean, eigenvectors, eigenvalues = cv2.PCACompute2(data_pts, mean)
 
-        # store the center of the object
+        # Store the center of the object
         cntr = (int(mean[0,0]), int(mean[0,1]))
-          ## [pca]
+        ## [pca]
+        a = math.atan2(eigenvectors[0,1], eigenvectors[0,0]) # orientation in radians
 
-          ## [visualization]
-          # draw the principal components
-        cv2.circle(canvas, cntr, 3, (255, 0, 255), 2)
         p1 = (cntr[0] + 0.02 * eigenvectors[0,0] * eigenvalues[0,0], cntr[1] + 0.02 * eigenvectors[0,1] * eigenvalues[0,0])
         p2 = (cntr[0] - 0.02 * eigenvectors[1,0] * eigenvalues[1,0], cntr[1] - 0.02 * eigenvectors[1,1] * eigenvalues[1,0])
 
+        #Visualisation
+        label = "  Rotation Angle: " + str(-int(np.rad2deg(a)) - 90) + " degrees"
+        textbox = cv2.rectangle(frame, (cntr[0], cntr[1]-25), (cntr[0] + 250, cntr[1] + 10), (255,255,255), -1)
+        cv2.putText(frame, label, (cntr[0], cntr[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+        drawAxis(frame, cntr, p1, (255, 255, 0), 1)
+        drawAxis(frame, cntr, p2, (0, 0, 255), 5)
+        cv2.drawContours(frame,[box],0,(255, 0,0),2)
+        cv2.circle(frame, (x,y), 10, (0,255,0), -5)
 
-        angle = atan2(eigenvectors[0,1], eigenvectors[0,0]) # orientation in radians
-          ## [visualization]
+        rotation = int(np.rad2deg(a)) + 90
 
-          # label with the rotation angle
-        label = "  Rotation Angle: " + str(int(np.rad2deg(angle)) + 90) + " degrees"
-        textbox = cv2.rectangle(canvas, (cntr[0], cntr[1]-25), (cntr[0] + 250, cntr[1] + 10), (255,255,255), -1)
-        cv2.putText(canvas, label, (cntr[0], cntr[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
-
-
-          # get angle
-        rotation = int(np.rad2deg(angle)) + 90
         if rotation >= 180:
             rotation=rotation-180
 
-        return angle
+        return rotation
+
 
     def search(self,threshold, straight_amount,detection_label): #recursive function to go in zig zags till gate detected
         #TODO MAKE STRAIGHT AMOUNT DETERMINED FROM OUR DEPTH TO SWEEP BOTTOM CAMERA FRAME
@@ -289,3 +279,20 @@ def agglomerative_cluster(contours, threshold_distance):
 
     return current_contours
 
+def drawAxis(img, p_, q_, colour, scale):
+    p = list(p_)
+    q = list(q_)
+    
+    angle = math.atan2(p[1] - q[1], p[0] - q[0]) # angle in radians
+    hypotenuse = math.sqrt((p[1] - q[1]) * (p[1] - q[1]) + (p[0] - q[0]) * (p[0] - q[0]))
+    # Here we lengthen the arrow by a factor of scale
+    q[0] = p[0] - scale * hypotenuse * math.cos(angle)
+    q[1] = p[1] - scale * hypotenuse * math.sin(angle)
+    cv2.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), colour, 1, cv2.LINE_AA)
+    # create the arrow hooks
+    p[0] = q[0] + 9 * math.cos(angle + math.pi / 4)
+    p[1] = q[1] + 9 * math.sin(angle + math.pi / 4)
+    cv2.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), colour, 1, cv2.LINE_AA)
+    p[0] = q[0] + 9 * math.cos(angle - math.pi / 4)
+    p[1] = q[1] + 9 * math.sin(angle - math.pi / 4)
+    cv2.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), colour, 1, cv2.LINE_AA)

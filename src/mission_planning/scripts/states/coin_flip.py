3e5381d4f35e562dd6b34890ff300a935aa9a3ce
dolf321 <dolfzigler@hotmail.com>
2022-07-29 07:59:27 +0400

    Changed coin flip to improve rotation accuracy drastically and avoid flickering aswell as using new search functions

--- src/mission_planning/scripts/states/coin_flip.py
+++ src/mission_planning/scripts/states/coin_flip.py
@@ -15,6 +15,8 @@ import math
 from transforms3d import euler
 import vision_msgs
 from calc_utils import is_approx_equal
+import actions_utils as au
+import threading
 
 class coin_flip(smach.State):
     _outcomes=['outcome1','outcome2']
@@ -22,103 +24,80 @@ class coin_flip(smach.State):
     _output_keys=[]
     def __init__(self,gate_label):   
         self.gate_label=gate_label
+
+    def rotate_cc(self): #rotate by 179 degrees
+        mc.mov_control.update_tf()
+
+        # Rotate from initial orientation
+        rotation = mc.mov_control.trans.transform.rotation
+        roll, pitch, yaw = euler.quat2euler([rotation.w, rotation.x, rotation.y, rotation.z], 'sxyz')
+        yaw=math.degrees(yaw)
+        rot_total = yaw + 179
+
+        print("ROTATING BY::")
+        print(math.degrees(mc.mov_control.to_euler(rot_total)))
+        mc.mov_control.set_rotation(roll,pitch,mc.mov_control.to_euler(rot_total))
+        
     def execute(self, userdata):     
         rospy.sleep(3)
 
         # Rotate from initial orientation
         mc.mov_control.update_tf()
         mc.mov_control.arm()
-        rotation = mc.mov_control.trans.transform.rotation
-        detected = False
-        detections = []
-        angle_sign = 0
-        center = (0.0, 0.0)
-        size = (0.0, 0.0)
-        angle_x, angle_y = 0.0, 0.0
-        roll, pitch, yaw = euler.quat2euler([rotation.w, rotation.x, rotation.y, rotation.z], 'sxyz')
-        for i in range(4):
-            print("ROTATING yaw:")
-            print(yaw)
-            print(yaw+math.pi/2*(i+1))
-            mc.mov_control.set_rotation(roll,pitch,yaw+math.pi/2*(i+1))
-            # Wait a bit for control to start 
-            rospy.sleep(0.1)
-            while(mc.mov_control.get_running_confirmation()):
-                if vs.front_camera.is_fetched:
-                    detections = vs.front_camera.get_detection([self.gate_label],0.05)
-                    # rospy.sleep(0.1) # delay already present in get_running_confirmation
-                    if len(detections) > 0: 
-                        # if(temp_detect!=None):
-                        #     detect=temp_detect
 
-                        # If area of bounding box too small (< 1/10 area of image): maybe not our gate
-                        if detections[0]['size'][0] * detections[0]['size'][1] < vs.front_camera.height * vs.front_camera.width / 12:
-                            print("Gate too small, ignoring...")
-                            continue
-                            
-                        # If center of bounding box is already at image center: no need for more analysis
-                        if detections[0]['center'][0] == vs.front_camera.get_center_coord()[0]:
-                            print("DETECTED AND EXISTS")
-                            mc.mov_control.stop()
-                            rospy.sleep(1)
+        self.rotate_cc()
 
-                            center = detections[0]['center']
-                            size = detections[0]['size']
-                            detected = True
-                            break
+        # Get dictionary of detections
+        detections_dict = {}
+        detections_dict[self.gate_label] = []  
 
-                        # First time seeing the gate: maybe at edge
-                        angle_x, angle_y = vs.front_camera.get_angles(detections[0]['center'][0], detections[0]['center'][1])
-                        # print(detections[0]['size'][0] * detections[0]['size'][1])
-                        # print("ANGLE_X")
-                        # print(angle_x)
-                        if angle_sign == 0: #only set when first starting
-                            print("Center of gate found...")
-                            angle_sign = int(angle_x / abs(angle_x))
+        # Get end time
+        end_time = rospy.get_time() + 60
+        
+        # Get a detection of the object
+        outcome = au.search_waiter(mc.mov_control, vs.front_camera, [self.gate_label], 5, 0.3, 
+                                0.1, detections_dict, end_time, lambda: True)
+        print("Search ended")
+        if outcome != "detected": 
+            print("NOT DETECTED")
+            return "outcome2"
 
-                        # if (is_approx_equal(detections[0]['center'][0], x_center, 0.4)): # or is_approx_equal(detect.bbox.center.y,y_center)):
-                        # Otherwise see if the gate angle sign changed (crossed the center) or near the center
-                        elif is_approx_equal(angle_x, 0) or int(angle_x / abs(angle_x)) == -angle_sign: #
-                            print("DETECTED AND EXISTS")
-                            mc.mov_control.stop()
-                            rospy.sleep(1)
-                            angle_x, angle_y = vs.front_camera.get_angles(detections[0]['center'][0], detections[0]['center'][1])
-                            
-                            center = detections[0]['center']
-                            size = detections[0]['size']
-
-                            detected = True
-                            break
-                
-            if (detected):
-                break     
-            
-            mc.mov_control.update_tf()
-
-        # vs.viso_control.reset_detection_of_interest()
-        if(detected):
-            mc.mov_control.update_tf()
-            altitude= mc.mov_control.trans.transform.translation.z
-            rotation = mc.mov_control.trans.transform.rotation
-            roll, pitch, yaw = euler.quat2euler([rotation.w, rotation.x, rotation.y, rotation.z], 'sxyz')
-
-
-
-            # Rotate and head thru center of the gate
-            angle_x, angle_y = vs.front_camera.get_angles(center[0], 0) #get angle between center of bbox and image frame
-            rospy.sleep(0.05)
-            mc.mov_control.set_rotation(0, 0, yaw - angle_x)
-            # Wait a bit for control to start 
-            rospy.sleep(0.1)
-            while (mc.mov_control.get_running_confirmation()):
-                rospy.sleep(0.001)
-            rospy.sleep(1)
+        if(self.recursive()):
             mc.mov_control.set_height(-0.8)
             mc.mov_control.await_completion()
-            # # Go straight
-            # mc.mov_control.go_straight(15)
-
             return 'outcome1'
-        else: 
+        else:
+            print("Couldnt align gate")
             return 'outcome2'
+
+    def recursive(self):
+        mc.mov_control.update_tf()
+        rotation = mc.mov_control.trans.transform.rotation
+        roll, pitch, yaw = euler.quat2euler([rotation.w, rotation.x, rotation.y, rotation.z], 'sxyz')
+        gate_detection = vs.front_camera.get_detection_t(self.gate_label,0.5)
+
+        if(len(gate_detection)==0):
+            print("Gate couldnt be detected for the first time")
+            return False
+
+        center = gate_detection[0]['center']
+
+        # Rotate to center of the gate
+        angle_x, angle_y = vs.front_camera.get_angles(center[0], 0) #get angle between center of bbox and image frame
+        mc.mov_control.rotate_ccw(-angle_x)
+
+        gate_detection = vs.front_camera.get_detection_t(self.gate_label,2)
+        if(len(gate_detection)==0):
+            print("Gate couldnt be detected a second time")
+            return False
+        center = gate_detection[0]['center']
+
+        # Rotate to center of the gate
+        angle_x_2, angle_y = vs.front_camera.get_angles(center[0], 0) #get angle between center of bbox and image frame
+        if(is_approx_equal(angle_x,angle_x_2,0.15)):
+            return True
+        else:
+            return self.recursive()
+            
+
         

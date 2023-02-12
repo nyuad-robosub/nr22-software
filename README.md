3cfa69a2754964d15f120c1727db3ef22c909200
dolf321 <dolfzigler@hotmail.com>
2023-02-12 01:44:46 -0500

    Added much more documentation to the readme, fixed file naming inconsistincies, improved bash script readability and fixed dependency on installation in home directory, added updated submodule for ORB_SLAM3_OPENCV4.

--- README.md
+++ README.md
@@ -1,9 +1,49 @@
-# nr22-software
-Repository for Robosub 2022 software team.
+<p align="center">
+  <a href="https://robosub.nyuad.io/">
+    <img
+      alt="NYUAD Robosub Logo"
+      src="https://robosub.nyuad.io/wp-content/uploads/2022/06/Dugong-lines_v12-1-2048x1307.png"
+      width="300"
+    />
+  </a>
+</p>
 
-The repository is structured as a catkin workspace with only the `/src` directory (ignoring all of the catkin files and folders).
+<div align="center">
+  <h1><a href="https://robosub.nyuad.io/"> NYUAD Robosub</a> | nr22-software</h1>
+  <p>
+    Repository used to operate the Dugong AUV in simulation and on-site operations for the NYUAD Robosub 2022 software team.
+  </p>
+</div>
 
-## Installing
+# Table of contents
+* [Repo Structure](#repository-structure)
+* [Installation/Setup](#installing)
+* [Alternative Installation/Setup](#installingalternative)
+* [Launching Simulation](#running-simulation-scripts)
+* [Launching ORB_SLAM3 with OAK-D](#launching-orb_slam3-with-the-oak-d)
+* [Installing ORB_SLAM2](#installing-orb_slam2-and-oakd-ros-launch-files)
+* [Launching ORB_SLAM2](#running-orb-slam2-and-oak-d-pipeline-scripts)
+# Repository Structure
+The repository is structured as a ROS catkin workspace with the following directories:
+
+    .
+    ├── include                 # Submodules and libraries that are not ROS packages needed to test and run the software
+    ├── src                     # Files containing all simulation and on-site scripts
+    │   ├── avoid_obstacles     # Catkin package: Path planning scripts based off pointclouds
+    │   ├── camera_controller   # Catkin package: OAK-D camera controller used to publish stereo camera data/swap calibration files
+    │   ├── color_detection     # Scripts used to detect colors underwater
+    │   ├── controls            # Catkin package: Sends telemetry data and TF2 frames of robot in space for navigation
+    │   ├── deps                # Repository submodules containing all external ROS package dependencies
+    │   ├── edge-gate-detection # Competition gate detection scripts
+    │   ├── faux_detection      # Catkin package: Sends machine learning object detections for simulation purposes.
+    │   ├── handle_detection    # Bin task handle detection scripts
+    │   ├── mission_planning    # Catkin package: Main mission planning scripts used for both simulation/on-site runs.
+    │   ├── pools               # Catkin package: Contains Gazebo world files for pool simulation
+    │   ├── sonar_ping          # Catkin package: Sends sonar pinger data for simulation
+    │   ├── torpedo-hole        # Torpedo task detection scripts
+    └── README.md
+
+# Installing
 
 - Clone the nr22-software and cd into it: (clone it outside of pre-existing workspaces, if any)
 
@@ -22,9 +62,11 @@ The repository is structured as a catkin workspace with only the `/src` director
 `git submodule update --recursive --remote`
 
 - If there are no pre-existing catkin workspaces (only ROS exists on the system): extend `nr22-software` to ROS Melodic install folder
+
   - `catkin config --init --extend /opt/ros/melodic`
 
 - If there is already a workspace (such as `~/gz_ws`) the extend above needs to be extending to that instead (note the `/devel` at the end). This is to "chain" all the workspaces, so that the build system can read all packages in all workspaces:
+
   - `catkin config --init --extend <path-to-last-workspace/devel>`
   - Example: `catkin config --init --extend ~/gz_ws/devel`
 
@@ -41,7 +83,7 @@ The repository is structured as a catkin workspace with only the `/src` director
   - Append `source <path-to-nr22-software>/nr22-software/devel/setup.bash` at the end of the file
   - This will allow each new terminal opened to recognize the new workspace and use the packages
 
-## Installing(Alternative)
+# Installing(Alternative)
 
 - Change directory to the new repo:
 
@@ -51,18 +93,33 @@ The repository is structured as a catkin workspace with only the `/src` director
 
 `sudo bash ./init_env.sh`
 
-## Installing ORB_SLAM2 and OAKD ros launch files
+> SVO installation will fail, install manually.
+
+# Running Simulation Scripts
+
+> Prior to running these scripts you should have run the init_env.sh file and [setup object detection here](src/faux_detection/README.md)
+- To run simulation, type `roslaunch mission_planning start_run_sim.launch`
+
+# Launching ORB_SLAM3 with the OAK-D
+
+- Simply start the launch file
+
+`roslaunch ORB_SLAM3 oakd_node.launch`
+
+# Installing ORB_SLAM2 and OAKD ros launch files
+
 
 - To setup ORB_SLAM2:
- - Go to `.bashrc` 
- - Append `export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:~/nr22-software/include/ORB_SLAM2/Examples/ROS/ORB_SLAM2`
+- Go to `.bashrc`
+- Append `export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:~/nr22-software/include/ORB_SLAM2/Examples/ROS/ORB_SLAM2`
 
-## Running ORB-SLAM2 and OAK-D pipeline scripts:
+# Running ORB-SLAM2 and OAK-D pipeline scripts:
 
 - Type `rosrun ORB_SLAM2 STEREO_OAKD {PATH TO VOCABULARY FILE} {PATH TO CAMERA YAML CONFIGURAITON FILE}`
   - For example: `rosrun ORB_SLAM2 STEREO_OAKD /home/rami/nr22-software/include/ORB_SLAM2/Vocabulary/ORBvoc.txt /home/rami/nr22-software/include/ORB_SLAM2/Examples/ROS/ORB_SLAM2/OAKD.yaml`
 
-## Creating new packages
+
+# Creating new packages
 
 - References: https://catkin-tools.readthedocs.io/en/latest/verbs/catkin_create.html
 
@@ -70,7 +127,7 @@ The repository is structured as a catkin workspace with only the `/src` director
 
 `cd src`
 
-- Use catkin-tools to create packages. This will create a new package folder in `/src` that has your necessary files. 
+- Use catkin-tools to create packages. This will create a new package folder in `/src` that has your necessary files.
 
 `catkin create pkg <package-name>`
 
@@ -80,8 +137,3 @@ The repository is structured as a catkin workspace with only the `/src` director
 
 - Refer to [this (C++)](http://wiki.ros.org/ROS/Tutorials/WritingPublisherSubscriber%28c%2B%2B%29) and [this (Python)](http://wiki.ros.org/ROS/Tutorials/WritingPublisherSubscriber%28python%29) to write your first nodes.
 
-## Launching ORB_SLAM3 with the OAK-D
-
-- Simply start the launch file 
-
-`roslaunch ORB_SLAM3 oakd_node.launch`
--- init_env.sh
+++ init_env.sh
@@ -1,203 +1,128 @@
+#!/bin/bash   
+# INSTALLS REQUIRED DEPENDENCIES AND BUILD CATKIN WORKSPACE
 
-echo "Building ROS nodes"
-# sudo apt update
-# sudo apt install python3-opencv
+nr_directory=$(pwd)
+include_directory="${nr_directory}/include"
 
-# sudo apt-get install build-essential
+# COLORS AND LOGGING FUNCTIONS
+MAGENTA = "\x1b[35m"
+NO_COLOR = "\x1b[0m"
+log () {
+    echo -e $MAGENTA "NR LOG: " $1 $NO_COLOR 
+}
 
-# sudo apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
-# #cd ./catkin_ws
-# p=$(pwd)
-# mkdir requiredlibs
-# cd requiredlibs
-
-# git clone --recursive https://github.com/stevenlovegrove/Pangolin.git
-# git clone https://gitlab.com/libeigen/eigen.git
-
-# # Get Pangolin
-# cd Pangolin 
-# mkdir build
-# cmake .. 
-# make
-
-# #cd $p
-# #cd ./requiredlibs/eigen
-
-# #mkdir build
-# #cd build
-# #cmake ..
-# #sudo make install 
-# #sudo apt install libeigen3-dev
-
-# # Install dependencies (as described above, or your preferred method)
-# ./scripts/install_prerequisites.sh recommended
-
-# # Configure and build
-# #mkdir build && cd build
-# #cmake ..
-# #cmake --build .
-
-# # GIVEME THE PYTHON STUFF!!!! (Check the output to verify selected python version)
-# #cmake --build . -t pypangolin_pip_install
-
-# # Run me some tests! (Requires Catch2 which must be manually installed on Ubuntu.)
-# #ctest
-
-
-# #FIX ORBSLAM ADD 
-# #include <unistd.h>
-# #include <stdio.h>
-# #include <stdlib.h>
-# cd $p
-# cd ./src
-# git clone https://github.com/nyuad-robosub/realsense-ros
-# git clone https://github.com/nyuad-robosub/ORB_SLAM2
-
-# sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key C8B3A55A6F3EFCDE
-# sudo add-apt-repository "deb https://librealsense.intel.com/Debian/apt-repo $(lsb_release -cs) main"
-# sudo apt-get update
-# sudo apt-get install librealsense2-dkms --allow-unauthenticated -y
-# sudo apt-get install librealsense2-dev --allow-unauthenticated -y
-# catkin config --extend /opt/ros/melodic
-# cd $p
-# catkin build || true
-# cd $p
-# source $p/devel/setup.bash
-
-#to run realsense roslaunch realsense2_camera rs_camera.launch filters:=pointcloud
-#rosrun ORB_SLAM2 RGBD ~catkin_ws/src/ORB_SLAM2/Vocabulary/ORBvoc.txt ~catkin_ws/src/ORB_SLAM2/Examples/ROS/ORB_SLAM2/Asus.yaml
-
+log "Building ROS nodes"
 git submodule update --init --recursive
 
-#install orbslam3 dependencies
-sudo apt install libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
-sudo apt install libgl1-mesa-dev libglew-dev cmake python3.6 libpython3.6-dev pkg-config libegl1-mesa-dev libwayland-dev libxkbcommon-dev wayland-protocols ffmpeg libavcodec-dev libavutil-dev libavformat-dev libswscale-dev libavdevice-dev libjpeg-dev libtiff5-dev libopenexr-dev python3-pip g++ git gcc ros-melodic-tf2-geometry-msgs
-sudo apt install libusb-1.0-0-dev
+log "Installing ROS"
+sudo apt-get install -y ros-melodic-desktop-full
+echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+sudo apt-get install -y ros-melodic-ddynamic-reconfigure ros-melodic-rgbd-launch ros-melodic-vision-msgs ros-melodic-camera-info-manager ros-melodic-pcl-ros ros-melodic-image-transport ros-melodic-tf2-sensor-msgs ros-melodic-smach ros-melodic-rosbuild ros-melodic-mavros nlohmann-json-dev
+
+# Install ORB-SLAM3 dependencies
+log "Installing ORB-SLAM3 dependencies"
+sudo apt install -y libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+sudo apt install -y libgl1-mesa-dev libglew-dev cmake python3.6 libpython3.6-dev pkg-config libegl1-mesa-dev libwayland-dev libxkbcommon-dev wayland-protocols ffmpeg libavcodec-dev libavutil-dev libavformat-dev libswscale-dev libavdevice-dev libjpeg-dev libtiff5-dev libopenexr-dev python3-pip g++ git gcc ros-melodic-tf2-geometry-msgs
+sudo apt install -y libusb-1.0-0-dev
+
 # Install realsense2 packages
+log "Installing realsense2 packages"
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key C8B3A55A6F3EFCDE
 sudo add-apt-repository "deb https://librealsense.intel.com/Debian/apt-repo $(lsb_release -cs) main"
 sudo apt-get update
-sudo apt-get install librealsense2-dkms --allow-unauthenticated -y
-sudo apt-get install librealsense2-dev --allow-unauthenticated -y
-sudo apt-get install librealsense2-utils --allow-unauthenticated -y
+sudo apt-get install -y librealsense2-dkms --allow-unauthenticated -y
+sudo apt-get install -y librealsense2-dev --allow-unauthenticated -y
+sudo apt-get install -y librealsense2-utils --allow-unauthenticated -y
 
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-get install -y curl # if you haven't already installed curl
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
-
-sudo apt-get install ros-melodic-desktop-full
-echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
-sudo apt-get install ros-melodic-ddynamic-reconfigure
-sudo apt install ros-melodic-rgbd-launch
-sudo apt-get install ros-melodic-vision-msgs
-sudo apt-get install ros-melodic-camera-info-manager
-sudo apt-get install ros-melodic-pcl-ros
-sudo apt-get install ros-melodic-image-transport
-sudo apt-get install ros-melodic-tf2-sensor-msgs
-sudo apt-get install ros-melodic-smach
-sudo apt-get install libbz2-dev
-sudo apt install nlohmann-json-dev
 
 cd ./include
 p=$(pwd)
 
 pip2 install opencv-contrib-python
 
-# # Get Pangolin
-# wget "https://codeload.github.com/stevenlovegrove/Pangolin/zip/refs/tags/v0.5"
-# unzip v0.5
-# rm v0.5
+# SVO dep installation
+sudo apt install -y libgoogle-glog-dev
+
+# Get Pangolin
 sudo apt-get install -y libglew-dev libboost-dev libboost-thread-dev libboost-filesystem-dev ffmpeg libavutil-dev libpng-dev
-cd $p/Pangolin*
-#./scripts/install_prerequisites.sh --dry-run recommended
+cd $include_directory/Pangolin*
 mkdir build
 cd build
 cmake .. 
 sudo make install
 
-# #FOR ORBSLAM WE NEED SOPHUS #include <sophus/se3.hpp>
-# cd $p/Sophus
+# #FOR ORBSLAM2 WE NEED SOPHUS #include <sophus/se3.hpp>
+# cd $include_directory/Sophus
 # mkdir build
 # cd build
 # cmake ..
 # sudo make install
 
-#FOR ORBSLAM WE NEED FMT #include <sophus/se3.hpp>
-cd $p/fmt
+#FOR ORBSLAM3 WE NEED FMT #include <sophus/se3.hpp>
+cd $include_directory/fmt
 mkdir build
 cd build
 cmake ..
 sudo make install
 
 #JSON for depthai-ros
-cd $p/json
+cd $include_directory/json
 mkdir build
 cd build
 cmake ..
 sudo make install
 
-#GET EIGEN 3.2.10 VERSION FOR NO CONFLICTS
-# wget "https://gitlab.com/libeigen/eigen/-/archive/3.2.10/eigen-3.2.10.tar.bz2"
-cd $p
-#wget "https://gitlab.com/libeigen/eigen/-/archive/3.3.9/eigen-3.3.9.tar.bz2"
-#tar -xf eigen-3.3.9.tar.bz2
-#rm eigen-3.3.9.tar.bz2
-
-#wget "https://gitlab.com/libeigen/eigen/-/archive/3.3.0/eigen-3.3.0.tar.bz2"
-#tar -xf eigen-3.3.0.tar.bz2
+# EIGEN Version 3.2.10 FOR NO CONFLICTS
+cd $include_directory
 wget "https://gitlab.com/libeigen/eigen/-/archive/3.2.10/eigen-3.2.10.tar.bz2"
 tar -xf eigen-3.2.10.tar.bz2
 rm eigen-3.2.10.tar.bz2
-cd $p/eigen*
+cd $include_directory/eigen*
 mkdir build
 cd build
 cmake ..
-#make
 sudo make install
 
-#install opencv 4
-echo "Configuring and building openCV for ORBSLAM3 and depthai-ros"
-cd $p/include
-wget -O opencv.zip https://github.com/opencv/opencv/archive/refs/tags/4.4.0.zip
-unzip opencv
-rm opencv.zip
-cd $p/opencv-4.4.0
-mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make
+# Install OpenCV 4.4(If you have dependency issues)
+# log "Configuring and building openCV for ORBSLAM3 and depthai-ros"
+# cd $include_directory
+# wget -O opencv.zip https://github.com/opencv/opencv/archive/refs/tags/4.4.0.zip
+# unzip opencv
+# rm opencv.zip
+# cd $include_directory/opencv-4.4.0
+# mkdir build
+# cd build
+# cmake .. -DCMAKE_BUILD_TYPE=Release
+# make
+# Alternatively....
+sudo apt install -y libopencv-dev python3-opencv
 
-#install depthai
-echo "Configuring/building depthai-core"
-cd $p/depthai-core
+# Install depthai-core (if this has any issues try clearing the .Hunter folder in home directory)
+log "Configuring/building depthai-core"
+cd $include_directory/depthai-core
 cmake -H. -Bbuild -D'BUILD_SHARED_LIBS=ON'
 cmake --build build
-#cd $p/depthai-core/build
-#sudo make install
 
-cd $p
-cd ..
-p=$(pwd)
+cd $nr_directory
 catkin config --extend /opt/ros/melodic
-#catkin build || true 
 catkin_make -j2
 
-source $p/devel/setup.sh
+source $nr_directory/devel/setup.sh
+echo $nr_directory'/devel/setup.sh' >> ~/.bashrc
 
 #create orbslam3 ros package separately
-#bash $p/include/ORB_SLAM3/build_ros.sh
-#bash $p/include/ORB_SLAM2/build_ros.sh
-bash $p/include/ORB_SLAM3_OPENCV4/build.sh
+cd $include_directory/ORB_SLAM3_OPENCV4/build.sh
+bash ./build.sh
 
-#you need to export in bash
-echo 'export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:~/nr22-software/include/ORB_SLAM3_OPENCV4/Examples/ROS/ORB_SLAM3' >> ~/.bashrc 
-echo 'source ~/nr22-software/devel/setup.bash' >> ~/.bashrc
-
-bash $p/include/ORB_SLAM3_OPENCV4/build_ros.sh
-#source /opt/ros/melodic/setup.bash
-#export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:~/nr22-software/include/ORB_SLAM2/Examples/ROS/ORB_SLAM2
-#roslaunch realsense2_camera rs_rgbd.launch initial_reset:=true
-#rosrun ORB_SLAM2 RGBD /home/rami/nr22-software/include/ORB_SLAM2/Vocabulary/ORBvoc.txt /home/rami/nr22-software/include/ORB_SLAM2/Examples/ROS/ORB_SLAM2/Asus.yaml
+# This is to add package path to bashrc for ROS support
+# Fix the path
+echo 'export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:'$include_directory'/ORB_SLAM3_OPENCV4/Examples/ROS/ORB_SLAM3' >> ~/.bashrc 
+source $nr_directory/devel/setup.bash
+echo 'source '$nr_directory'/devel/setup.bash' >> ~/.bashrc
+bash ./build_ros.sh
 
 # CXX version fix 
 # sed -i 's/++11/++14/g' CMakeLists.txt

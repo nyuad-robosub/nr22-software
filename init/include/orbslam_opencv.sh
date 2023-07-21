3d0a56e979fb3107cd622b3306baa87750d19aca
quannham <nhamngocminhquan@gmail.com>
2023-07-20 22:30:20 -0400

    Update orbslam_opencv.sh: remove unused operations

--- init/include/orbslam_opencv.sh
+++ init/include/orbslam_opencv.sh
@@ -12,12 +12,12 @@ log () {
 }
 
 log "Building ROS nodes"
-git submodule update --init --recursive
+# git submodule update --init --recursive
 
-log "Installing ROS"
-sudo apt-get install -y ros-melodic-desktop-full
-echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
-sudo apt-get install -y ros-melodic-ddynamic-reconfigure ros-melodic-rgbd-launch ros-melodic-vision-msgs ros-melodic-camera-info-manager ros-melodic-pcl-ros ros-melodic-image-transport ros-melodic-tf2-sensor-msgs ros-melodic-smach ros-melodic-rosbuild ros-melodic-mavros nlohmann-json-dev
+# log "Installing ROS"
+# sudo apt-get install -y ros-melodic-desktop-full
+# echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+# sudo apt-get install -y ros-melodic-ddynamic-reconfigure ros-melodic-rgbd-launch ros-melodic-vision-msgs ros-melodic-camera-info-manager ros-melodic-pcl-ros ros-melodic-image-transport ros-melodic-tf2-sensor-msgs ros-melodic-smach ros-melodic-rosbuild ros-melodic-mavros nlohmann-json-dev
 
 # Install ORB-SLAM3 dependencies
 log "Installing ORB-SLAM3 dependencies"
@@ -25,18 +25,9 @@ sudo apt install -y libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libs
 sudo apt install -y libgl1-mesa-dev libglew-dev cmake python3.6 libpython3.6-dev pkg-config libegl1-mesa-dev libwayland-dev libxkbcommon-dev wayland-protocols ffmpeg libavcodec-dev libavutil-dev libavformat-dev libswscale-dev libavdevice-dev libjpeg-dev libtiff5-dev libopenexr-dev python3-pip g++ git gcc ros-melodic-tf2-geometry-msgs
 sudo apt install -y libusb-1.0-0-dev
 
-# Install realsense2 packages
-log "Installing realsense2 packages"
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key C8B3A55A6F3EFCDE
-sudo add-apt-repository "deb https://librealsense.intel.com/Debian/apt-repo $(lsb_release -cs) main"
-sudo apt-get update
-sudo apt-get install -y librealsense2-dkms --allow-unauthenticated -y
-sudo apt-get install -y librealsense2-dev --allow-unauthenticated -y
-sudo apt-get install -y librealsense2-utils --allow-unauthenticated -y
-
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-get install -y curl # if you haven't already installed curl
-curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+# sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+# sudo apt-get install -y curl # if you haven't already installed curl
+# curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 
 cd ./include
 p=$(pwd)
@@ -106,12 +97,13 @@ cd $include_directory/depthai-core
 cmake -H. -Bbuild -D'BUILD_SHARED_LIBS=ON'
 cmake --build build
 
-cd $nr_directory
-catkin config --extend /opt/ros/melodic
-catkin_make -j2
+# cd $nr_directory
+# catkin config --extend /opt/ros/melodic
+# catkin_make -j2
+catkin build
 
-source $nr_directory/devel/setup.sh
-echo $nr_directory'/devel/setup.sh' >> ~/.bashrc
+# source $nr_directory/devel/setup.sh
+# echo $nr_directory'/devel/setup.sh' >> ~/.bashrc
 
 #create orbslam3 ros package separately
 cd $include_directory/ORB_SLAM3_OPENCV4/build.sh
@@ -120,9 +112,11 @@ bash ./build.sh
 # This is to add package path to bashrc for ROS support
 # Fix the path
 echo 'export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:'$include_directory'/ORB_SLAM3_OPENCV4/Examples/ROS/ORB_SLAM3' >> ~/.bashrc 
-source $nr_directory/devel/setup.bash
-echo 'source '$nr_directory'/devel/setup.bash' >> ~/.bashrc
+# source $nr_directory/devel/setup.bash
+# echo 'source '$nr_directory'/devel/setup.bash' >> ~/.bashrc
 bash ./build_ros.sh
+
+cd $nr_directory
 
 # CXX version fix 
 # sed -i 's/++11/++14/g' CMakeLists.txt
